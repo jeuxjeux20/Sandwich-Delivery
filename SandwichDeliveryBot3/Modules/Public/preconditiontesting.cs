cb1b831eba8c760390c485c04291de03bd2e1a97
Fires1 <samuelcadams02@gmail.com>
2017-06-23 14:29:15 -0500

    Testing command

---
+++ SandwichDeliveryBot3/Modules/Public/preconditiontesting.cs
@@ -0,0 +1,26 @@
+﻿//using Discord.Commands;
+//using SandwichDeliveryBot3.Precons;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace SandwichDeliveryBot3.Modules.Public
+//{
+//    public class preconditiontesting : ModuleBase
+//    {
+//        [Command("bl")]
+//        [NotBlacklisted]
+//        public async Task BlacklistPreconTest()
+//        {
+//            await ReplyAsync("passed with no errors.");
+//        }
+//        [Command("rsa")]
+//        [RequireSandwichArtist]
+//        public async Task ArtistPreconTest()
+//        {
+//            await ReplyAsync("passed with no errors.");
+//        }
+//    }
+//}
