9b69c0bb18cded7483127a3a62108aff5bdcbcfb
Fires1 <samuelcadams02@gmail.com>
2017-07-04 13:04:50 -0500

    New artist ranks!

--- SandwichDeliveryBot3/Enums/ArtistStatChange.cs
+++ SandwichDeliveryBot3/Enums/ArtistStatChange.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace SandwichDeliveryBot3.Enums
 {
+    //Remove
     public enum ArtistStatChange
     {
         Increase,
--- SandwichDeliveryBot3/Enums/ArtistStatusEnum.cs
+++ SandwichDeliveryBot3/Enums/ArtistStatusEnum.cs
@@ -2,9 +2,11 @@
 {
     public enum ArtistStatus
     {
-        Trainee,
-        Artist,
-        MasterArtist,
-        GodArtist
+        Trainee,//0-15
+        StandardArtist,//15 - 50
+        AboveAverage, //50 - 100
+        Master, //100 - 200
+        Veteran, //300 - 500
+        God //500+
     }
 }
